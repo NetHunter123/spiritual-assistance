21a22008d8509ceceb84831cbf005a85e6cec357
NetHunter123 <viktor3@i.ua>
2023-12-21 11:54:08 +0200

    upd btn

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -89,8 +89,8 @@ const Hero = () => {
                 <div className="relative z-10">
                   <Button
                     text={"Book a session!"}
-                    link={"https://wa.me/77751925078"}
-                    // link={"https://t.me/+919668310153"}
+                    // link={"https://wa.me/77751925078"}
+                    link={"https://t.me/+919668310153"}
                     place={"hero"}
                   />
                 </div>
